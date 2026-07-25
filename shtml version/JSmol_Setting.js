2b7ab2ee0f1bf8f7d3f6ad8d693e35fba433195b
gutow <gutow@uwosh.edu>
2026-07-25 15:30:03 -0500

    All JSmol settings to JSmol_Setting.js

---
+++ shtml version/JSmol_Setting.js
@@ -0,0 +1,12 @@
+var VSEPR_Jmol; //used to hold the applet.
+Jmol.Info = {
+      j2sPath: "../../Applets/JSmol/j2s",
+      use: "HTML5",
+      debug: false,
+      width: "98%",
+      height: "98%",
+	  disableJ2SLoadMonitor: false,
+	  disableInitialConsole: true,
+	  serverURL: "../../Applets/JSmol/php/jsmol.php",
+	  script: "set echo middle; color echo blue; set echo on; echo Use the menus and tools |at right (widescreen) or|below (tall screen) to display| and adjust a 3D image|you can manipulate.;"
+    }
